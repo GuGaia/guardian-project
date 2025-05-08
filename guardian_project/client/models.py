c5a37ac654e5a389515346f632029826fb9f9258
Leonardo Vinicius Wanderley Jatobá da Silva <lvwjs@ic.ufal.br>
2025-05-08 08:20:34 -0300

    add contact model

--- guardian_project/client/models.py
+++ guardian_project/client/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Clients(models.Model):
+class Client(models.Model):
     name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=255)
---
+++ guardian_project/contact/models.py
@@ -0,0 +1,19 @@
+# contacts/models.py
+from django.db import models
+from django.contrib.postgres.fields import ArrayField
+
+class Client(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Contact(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='contacts')
+    name = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=20)
+    plataform = models.CharField(max_length=20)
+    relationship = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'{self.name} ({self.client.name})'
