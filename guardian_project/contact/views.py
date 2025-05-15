009cd1c2333cc813c97d9d7c43b633a5f838d3f2
mauriciomateus02 <mauriciomateus02@gmail.com>
2025-05-15 09:14:14 -0300

    ajustado o delete

--- guardian_project/contact/views.py
+++ guardian_project/contact/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets
 from .models import Contact
 from .serializers import ContactSerializer
+from rest_framework.response import Response
+from rest_framework import status
 
 class ContactViewSet(viewsets.ModelViewSet):
     serializer_class = ContactSerializer
@@ -8,3 +10,16 @@ class ContactViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         client_pk = self.kwargs.get('client_pk')
         return Contact.objects.filter(client_id=client_pk)
+    def destroy(self, request, *args, **kwargs):
+        try:
+            instance = self.get_object()
+            self.perform_destroy(instance)
+            return Response(
+                {"success": True, "message": "Contato excluído com sucesso."},
+                status=status.HTTP_204_NO_CONTENT
+            )
+        except Exception as e:
+            return Response(
+                {"success": False, "message": "Erro ao excluir o contato.", "error": str(e)},
+                status=status.HTTP_400_BAD_REQUEST
+            )
