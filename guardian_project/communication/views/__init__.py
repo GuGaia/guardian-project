0a409bc6631781e67aa26cda5304027d851d00fa
Leonardo Vinicius Wanderley Jatobá da Silva <lvwjs@ic.ufal.br>
2025-05-25 22:46:10 -0300

    wip channel send

--- guardian_project/communication/views/__init__.py
+++ guardian_project/communication/views/__init__.py
@@ -1,9 +1,11 @@
 from .channel_views import CommunicationViewSet
 from .email_views import EmailView
 from .sms_views import SmsView
+from .channel_send_views import ChannelSendView
 
 __all__ = [
     'CommunicationViewSet',
     'EmailView',
     'SmsView',
+    'ChannelSendView',
 ]
