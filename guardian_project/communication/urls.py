fef5b103004c489378e82210c9efccc6cd401bb7
Leonardo Vinicius Wanderley Jatobá da Silva <lvwjs@ic.ufal.br>
2025-05-26 22:28:04 -0300

    wip

--- guardian_project/communication/urls.py
+++ guardian_project/communication/urls.py
@@ -2,21 +2,11 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import (
     CommunicationViewSet,
-    EmailView,
-    SmsView,
     ChannelSendView,
 )
 
 router = DefaultRouter()
 router.register(r'', CommunicationViewSet)  # rota: /api/clients/
-
-email_patterns = [
-    path('send/', EmailView.as_view(), name='send-email'),
-]
-
-sms_patterns = [
-    path('send/', SmsView.as_view(), name='send-sms'),
-]
 
 channel_send_patterns = [
     path('send/', ChannelSendView.as_view(), name='channel-send'),
@@ -24,7 +14,5 @@ channel_send_patterns = [
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('emails/', include(email_patterns)),
-    path('sms/', include(sms_patterns)),
-    path('channel_send/', include(channel_send_patterns)),
+    path('alert/', include(channel_send_patterns)),
 ]
--- guardian_project/communication/views/__init__.py
+++ guardian_project/communication/views/__init__.py
@@ -1,11 +1,7 @@
 from .channel_views import CommunicationViewSet
-from .email_views import EmailView
-from .sms_views import SmsView
 from .channel_send_views import ChannelSendView
 
 __all__ = [
     'CommunicationViewSet',
-    'EmailView',
-    'SmsView',
     'ChannelSendView',
 ]
