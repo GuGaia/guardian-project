6dbd172bfcbbd8544c0e9ebdf7b77e791ce234bf
Leonardo Vinicius Wanderley Jatobá da Silva <lvwjs@ic.ufal.br>
2025-05-27 22:18:34 -0300

    fix views

--- guardian_project/client/views.py
+++ guardian_project/client/views.py
@@ -21,3 +21,22 @@ class ClientViewSet(viewsets.ModelViewSet):
         clients = self.get_queryset()
         serializer = self.get_serializer(clients, many=True)
         return Response(serializer.data)
+
+    def put(self, request, *args, **kwargs):
+        auth_header = request.headers.get('Authorization', '')
+        token = auth_header.replace('Bearer ', '')
+        user_data = validate_token(token)
+
+        if not user_data:
+            return Response({'detail': 'Unauthorized'}, status=401)
+
+        try:
+            client = Client.objects.get(id=user_data['sub'])
+        except Client.DoesNotExist:
+            return Response({'detail': 'Not Found'}, status=404)
+
+        serializer = ClientSerializer(client, data=request.data, partial=True)
+        if serializer.is_valid():
+            updated_client = serializer.save()
+            return Response(ClientSerializer(updated_client).data, status=200)
+        return Response(serializer.errors, status=400)
