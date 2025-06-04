849b7817afb5648fffb8b916acb304a30e6a31d9
Gustavo Gaia <gu.gaia16@outlook.com>
2025-06-04 20:00:08 -0300

    Debug login error temporariamente correção de erro

--- guardian_project/auth_app/urls.py
+++ guardian_project/auth_app/urls.py
@@ -2,9 +2,8 @@
 
 from django.urls import path
 from .views import login_view
-from .views import init_admin
 
 urlpatterns = [
     path('login/', login_view, name='login'),
-    path('init-admin/', init_admin),
+    # path('init-admin/', init_admin),
 ]
